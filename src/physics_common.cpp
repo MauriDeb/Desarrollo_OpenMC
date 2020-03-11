21d702b1bd1efaeb5c508379942a4175a5749333
Mauri.Deb <medebarbora@gmail.com>
2020-03-11 12:07:41 -0300

    Esta es la version con geometry splitting implementada, usada para hacer todos las corridas utilizadas hasta la charla de avance

---
+++ src/physics_common.cpp
@@ -0,0 +1,40 @@
+#include "openmc/physics_common.h"
+
+#include "openmc/settings.h"
+#include "openmc/random_lcg.h"
+
+namespace openmc {
+
+//==============================================================================
+// RUSSIAN_ROULETTE
+//==============================================================================
+
+void russian_roulette(Particle* p)
+{
+  if (p->wgt_ < settings::weight_cutoff) {
+    if (prn() < p->wgt_ / settings::weight_survive) {
+      p->wgt_ = settings::weight_survive;
+      p->wgt_last_ = p->wgt_;
+    } else {
+      p->wgt_ = 0.;
+      p->wgt_last_ = 0.;
+      p->alive_ = false;
+    }
+  }
+}
+
+void russian_roulette_importance(Particle* p)
+{
+  if (p->wgt_ < settings::weight_cutoff / p->imp_ ) {
+    if (prn() < p->wgt_ / settings::weight_survive) {
+      p->wgt_ = settings::weight_survive;
+      p->wgt_last_ = p->wgt_;
+    } else {
+      p->wgt_ = 0.;
+      p->wgt_last_ = 0.;
+      p->alive_ = false;
+    }
+  }
+}
+
+} //namespace openmc
